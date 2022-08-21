2db5c41ea6cd5d70575e30c111fa675956d3d211
valentin marturet <valen.martu2206@gmail.com>
2022-08-21 20:16:28 -0300

    centrando detalles

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Layout from "../../Components/Layout/Layout";
 import useImage from "../../Hooks/useImage";
+import styles from "./ProductDetails.module.css";
 
 const ProductDetails = () => {
   const { productId } = useParams();
@@ -13,12 +14,14 @@ const ProductDetails = () => {
 
   return (
     <Layout>
-      <img src={image} />
-      <h1>{product.title}</h1>
-      <h3>{product.description}</h3>
-      <h3>${product.price}</h3>
-      <h3>Tamaños disponibles: {product.availableSizes}</h3>
-      <button onClick={() => console.log(product)}>Test</button>
+      <div className={styles.container}>
+        <img src={image} />
+        <h1>{product.title}</h1>
+        <h3>{product.description}</h3>
+        <h3>${product.price}</h3>
+        <h3>Tamaños disponibles: {product.availableSizes}</h3>
+        <button onClick={() => console.log(product)}>Test</button>
+      </div>
     </Layout>
   );
 };
