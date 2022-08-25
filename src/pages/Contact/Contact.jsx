7bd52ce210f8e9fb61888a6a3e41a1d9859edd93
valentin marturet <valen.martu2206@gmail.com>
2022-08-24 21:51:32 -0300

    retocando pagina de contacto

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ContactForm from '../../Components/Contact/ContactForm/ContactForm'
-import Layout from '../../Components/Layout/Layout'
+import React from "react";
+import ContactForm from "../../Components/Contact/ContactForm/ContactForm";
+import Layout from "../../Components/Layout/Layout";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
   return (
     <Layout>
-        <ContactForm />
+      <h1 className={styles.title}>
+        Envianos un mensaje y nos pondremos en contacto lo mas pronto posible
+      </h1>
+      <ContactForm />
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
