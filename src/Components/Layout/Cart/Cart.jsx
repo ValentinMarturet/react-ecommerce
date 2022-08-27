e459fec90120efbe3a8def95dad5dd08c7e8c71a
valentin marturet <valen.martu2206@gmail.com>
2022-08-26 21:54:37 -0300

    haciendo cambios de estilo al carrito

--- src/Components/Layout/Cart/Cart.jsx
+++ src/Components/Layout/Cart/Cart.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import CartCard from "../CartCard/CartCard";
 import styles from "./Cart.module.css";
 import { useNavigate } from "react-router-dom";
+import { toggle } from "../../../reducers/cartSlice";
 
 const Cart = () => {
   const { isShowing, cartItems } = useSelector((state) => state.cart);
@@ -15,28 +16,43 @@ const Cart = () => {
     }
   };
 
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
 
   return (
     <div className={`${styles.cart} ${showCart()}`}>
-      {!cartItems[0] && (
-        <h1 className={styles.empty_text}>
-          Aun no has agregado nada al carrito
-        </h1>
-      )}
-      {cartItems &&
-        cartItems.map((element) => {
-          const index = products.map((item) => item.id).indexOf(element.id);
-          return (
-            <CartCard
-              name={products[index].title}
-              img={products[index].sku}
-              id={element.id}
-              cant={element.quantity}
-            />
-          );
-        })}
-      <button onClick={() => navigate("/checkout")}>checkout</button>
+      <div className={styles.card_container}>
+        {!cartItems[0] && (
+          <h1 className={styles.empty_text}>
+            Aun no has agregado nada al carrito
+          </h1>
+        )}
+        {cartItems &&
+          cartItems.map((element) => {
+            const index = products.map((item) => item.id).indexOf(element.id);
+            return (
+              <CartCard
+                name={products[index].title}
+                img={products[index].sku}
+                id={element.id}
+                cant={element.quantity}
+              />
+            );
+          })}
+        <div style={{ width: "350px", height: "70px" }}></div>
+      </div>
+      <div className={styles.btn_container}>
+        <button
+          className={styles.checkout_btn}
+          onClick={() => {
+            navigate("/checkout");
+            dispatch(toggle());
+          }}
+        >
+          Comprar
+        </button>
+      </div>
     </div>
   );
 };
--- src/Components/Layout/CartCard/CartCard.jsx
+++ src/Components/Layout/CartCard/CartCard.jsx
@@ -18,17 +18,19 @@ const CartCard = ({ name, img, id, cant }) => {
       )}
       <div className={styles.info_container}>
         <h2 className={styles.name}>{name}</h2>
+      </div>
+      <div className={styles.info_end}>
+        <button
+          className={styles.close_btn}
+          onClick={() => {
+            console.log("delete");
+            dispatch(deleteItem({ id: id }));
+          }}
+        >
+          x
+        </button>
         <p className={styles.quantity}>x {cant}</p>
       </div>
-      <button
-        className={styles.close_btn}
-        onClick={() => {
-          console.log("delete");
-          dispatch(deleteItem({ id: id }));
-        }}
-      >
-        x
-      </button>
     </div>
   );
 };
